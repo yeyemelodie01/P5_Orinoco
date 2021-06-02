559dac52f2f1a27c53b56585cbaaac19a552e35b
melodieyeye <yeyemelodie@outlook.fr>
2021-06-02 17:26:53 +0200

    modification page produit

--- produit.js
+++ produit.js
@@ -23,21 +23,20 @@ function renderProduct()
                 return resp.json();
             })
             .then(function (product) {
-                console.log(product);
                 let divfigure = document.createElement("div");
-                    divfigure.classList.add("divfigure");
+                divfigure.classList.add("divfigure");
 
                 let figureimg = document.createElement("figure");
-                    figureimg.classList.add("figure_img");
-                    divfigure.appendChild(figureimg);
+                figureimg.classList.add("figure_img");
+                divfigure.appendChild(figureimg);
 
                 let imgpro = document.createElement("img");
-                    imgpro.src = product.imageUrl;
-                    figureimg.appendChild(imgpro);
+                imgpro.src = product.imageUrl;
+                figureimg.appendChild(imgpro);
 
                 let divtext = document.createElement("div");
-                    divtext.classList.add("section-text");
-                    divtext.classList.add("pl-6");
+                divtext.classList.add("section-text");
+                divtext.classList.add("pl-6");
 
                 let title = document.createElement("h1");
                 title.classList.add("title");
@@ -72,81 +71,18 @@ function renderProduct()
                 }
 
                 let colors = product.colors;
-                if (typeof colors != 'undefined') {
-                    let color = document.createElement("h3");
-                    color.classList.add("title");
-                    color.classList.add("is-3");
-                    color.classList.add("mb-3");
-                    color.appendChild(document.createTextNode("Couleur"));
-                    divtext.appendChild(color);
-
-                    let colorDiv = document.createElement("div");
-                    colorDiv.classList.add("select");
-                    colorDiv.classList.add("is-primary");
-                    colorDiv.classList.add("mb-6");
-                    divtext.appendChild(colorDiv);
-
-                    let colorSelect = document.createElement("select");
-                    colorSelect.id = 'colors';
-                    colorDiv.appendChild(colorSelect);
-                    for (let i = 0; i < colors.length; ++i) {
-                        let selectOption = document.createElement("option");
-                        selectOption.text = colors[i];
-                        selectOption.value = colors[i];
-                        colorSelect.add(selectOption);
-                    }
+                if (typeof colors != 'undefined'){
+                    createDetailByType(colors, divtext, 'Couleurs', 'colors');
                 }
 
-                let varnishs = product.varnish;
-                if (typeof varnishs != 'undefined'){
-                    let vernis = document.createElement("h3");
-                    vernis.classList.add("title");
-                    vernis.classList.add("is-3");
-                    vernis.classList.add("mb-3");
-                    vernis.appendChild(document.createTextNode("Vernis"));
-                    divtext.appendChild(vernis);
-
-                    let vernisDiv = document.createElement("div");
-                    vernisDiv.classList.add("select");
-                    vernisDiv.classList.add("is-primary");
-                    vernisDiv.classList.add("mb-6");
-                    divtext.appendChild(vernisDiv);
-
-                    let vernisSelect = document.createElement("select");
-                    vernisSelect.id = 'vernis';
-                    vernisDiv.appendChild(vernisSelect);
-                    for (let i = 0; i < varnishs.length; ++i) {
-                        let vernisOption = document.createElement("option");
-                        vernisOption.text = varnishs[i];
-                        vernisOption.value = varnishs[i];
-                        vernisSelect.add(vernisOption);
-                    }
+                let varnish = product.varnish;
+                if (typeof varnish != 'undefined'){
+                    createDetailByType(varnish, divtext, 'Vernis', 'varnish');
                 }
 
-                let lense = product.lenses;
-                if (typeof lense != 'undefined'){
-                    let lentilles = document.createElement("h3");
-                    lentilles.classList.add("title");
-                    lentilles.classList.add("is-3");
-                    lentilles.classList.add("mb-3");
-                    lentilles.appendChild(document.createTextNode("Lentilles"));
-                    divtext.appendChild(lentilles);
-
-                    let lentillesDiv = document.createElement("div");
-                    lentillesDiv.classList.add("select");
-                    lentillesDiv.classList.add("is-primary");
-                    lentillesDiv.classList.add("mb-6");
-                    divtext.appendChild(lentillesDiv);
-
-                    let lentillesSelect = document.createElement("select");
-                    lentillesSelect.id = 'lentilles';
-                    lentillesDiv.appendChild(lentillesSelect);
-                    for (let i = 0; i < lense.length; ++i) {
-                        let lentillesOption = document.createElement("option");
-                        lentillesOption.text = lense[i];
-                        lentillesOption.value = lense[i];
-                        lentillesSelect.add(lentillesOption);
-                    }
+                let lenses = product.lenses;
+                if (typeof lenses != 'undefined'){
+                    createDetailByType(lenses, divtext, 'Lentilles', 'lenses');
                 }
 
                 let quantityTitle = document.createElement("h3");
@@ -174,7 +110,6 @@ function renderProduct()
                     quantitySelect.add(quantityOption);
                 }
 
-
                 let addToCartButton = document.createElement("button");
                 addToCartButton.classList.add("button");
                 addToCartButton.classList.add("is-primary");
@@ -186,111 +121,114 @@ function renderProduct()
                 addToCartButton.addEventListener('click', (event) =>{
                     event.preventDefault();
 
-                    let itemQuantity = document.getElementById('quantity').value;
-                    let panier =
-                        [{
-                            'id' : product._id,
-                            'type' : type,
-                            'color' : product.colors || product.varnish || product.lenses,
-                            'quantity' :itemQuantity,
-                            'price' : String(product.price),
-                            'totalPrice' : String(product.price * itemQuantity)
-                        }]
-                        console.log(panier)
-
-                    let data = JSON.parse(localStorage.getItem("basket"))
-                    if(data) {
-                        data.push[panier];
-                        localStorage.setItem("basket", JSON.stringify([panier]));
-                        console.log(data);
-                    } else {
-                        let tab = [];
-                        tab.push[panier];
-                        localStorage.setItem("basket", JSON.stringify(tab));
+                    let itemQuantity = parseInt(document.getElementById('quantity').value);
+                    let id = '';
+                    if (product.colors) {
+                        id = 'color';
+                    }else if(product.varnish) {
+                        id = 'varnish';
+                    } else if(product.lenses) {
+                        id = 'lenses';
                     }
 
-                    /*localStorage.setItem('type', type);
-                    localStorage.setItem('quantity', itemQuantity);
-                    localStorage.setItem('price',String(product.price));
-                    localStorage.setItem('totalPrice', String(product.price * itemQuantity));*/
+                    let detail = document.getElementById(id).value;
+                    if (itemQuantity > 0) {
+                        let itemToAddInBasket = {
+                            'id': product._id,
+                            'type': type,
+                            'detail': detail,
+                            'quantity': itemQuantity,
+                            'price':  parseInt(product.price),
+                            'totalPrice': parseInt(product.price * itemQuantity)
+                        };
+                        addToBasket(itemToAddInBasket)
+                    } else if (itemQuantity === 0 ) {
+                        let itemToRemove = {
+                            'id': product._id,
+                            'type': type,
+                            'detail': detail,
+                        }
+                        removeInBasket(itemToRemove)
+                    }
                 });
 
                 let description = document.createElement("h3");
-                    description.classList.add("title");
-                    description.classList.add("is-3");
-                    description.classList.add("mb-3");
-                    description.appendChild(document.createTextNode("Description :"));
-                    divtext.appendChild(description);
+                description.classList.add("title");
+                description.classList.add("is-3");
+                description.classList.add("mb-3");
+                description.appendChild(document.createTextNode("Description :"));
+                divtext.appendChild(description);
 
                 let text = document.createElement("p");
-                    text.classList.add("subtitle");
-                    text.classList.add("is-5");
-                    text.classList.add("has-text-justified");
-                    text.classList.add("mt-2");
-                    text.appendChild(document.createTextNode(product.description));
-                    divtext.appendChild(text);
+                text.classList.add("subtitle");
+                text.classList.add("is-5");
+                text.classList.add("has-text-justified");
+                text.classList.add("mt-2");
+                text.appendChild(document.createTextNode(product.description));
+                divtext.appendChild(text);
 
-
-
-                    let sectionpro = document.getElementById("products");
-                        sectionpro.appendChild(divfigure);
-                        sectionpro.appendChild(divtext);
+                let sectionpro = document.getElementById("products");
+                sectionpro.appendChild(divfigure);
+                sectionpro.appendChild(divtext);
             })
     }
 }
 
+function createDetailByType(item, parentDiv, title, id)
+{
+    let itemTitle = document.createElement("h3");
+    itemTitle.classList.add("title");
+    itemTitle.classList.add("is-3");
+    itemTitle.classList.add("mb-3");
+    itemTitle.appendChild(document.createTextNode(title));
+    parentDiv.appendChild(itemTitle);
 
-    /*
-    <div class="section-flex-div">
-        <figure class="figure_img">
-          <img src="images/teddy_1.jpg" alt="Norbert">
-        </figure>
-      </div>
-      <div class="section_text">
-        <h1 class="title is-1 mb-5">Robert</h1>
-        <h2 class="section_text-h2 title is-2 is-flex is-justify-content-flex-start mb-5">29€</h2>
-        <div class="icon-star icon-star-product mb-5">
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-        </div>
-        <h3 class="title is-3 mb-3">Couleur</h3>
-        <div class="select is-primary mb-6">
-          <select>
-            <option>...</option>
-            <option>Tan</option>
-            <option>Chocolate</option>
-            <option>Black</option>
-            <option>White</option>
-          </select>
-        </div>
-        <h3 class="title is-3 mb-3">Quantité</h3>
-        <div class="select is-primary mb-6">
-          <select>
-            <option>0</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-            <option>8</option>
-            <option>9</option>
-            <option>10</option>
-          </select>
-        </div>
-        <button class="button is-primary ml-5">Ajouter au panier</button>
-        <h3 class="title is-3 mb-3">Description :</h3>
-        <p class="subtitle is-5 has-text-justified mt-2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dui mauris, dignissim a blandit at, vehicula et mauris.
-          Mauris varius accumsan tristique. Integer ex felis, mattis sit amet accumsan quis, tempus vitae lorem.
-          Quisque at lacus pellentesque justo mollis porttitor a ac tellus.
-          Phasellus volutpat porta orci, vitae egestas felis posuere et. Nullam sed lobortis mi.
-          Nunc posuere ipsum tempor, sagittis purus vel, consequat magna. Ut finibus lacinia enim et sagittis.
-          Suspendisse luctus orci non metus imperdiet facilisis. Mauris eget libero lacinia, pulvinar sem et, fermentum nulla.
-          Morbi ut risus id sapien malesuada aliquet at in metus. Nulla consectetur tincidunt consequat.
-          Nunc eleifend nulla dui, id finibus elit maximus eget. </p>
-      </div>*/
+    let itemDiv = document.createElement("div");
+    itemDiv.classList.add("select");
+    itemDiv.classList.add("is-primary");
+    itemDiv.classList.add("mb-6");
+    parentDiv.appendChild(itemDiv);
+
+    let itemSelect = document.createElement("select");
+    itemSelect.id = id;
+    itemDiv.appendChild(itemSelect);
+    for (let i = 0; i < item.length; ++i) {
+        let itemOption = document.createElement("option");
+        itemOption.text = item[i];
+        itemOption.value = item[i];
+        itemSelect.add(itemOption);
+    }
+}
+
+function addToBasket(itemToAdd)
+{
+    let data = JSON.parse(localStorage.getItem("basket"))
+    if(!data) {
+        data = [];
+    }
+    let allReadyInBasket = false;
+    if (data !== []) {
+        for (let i = 0; i < data.length; ++i) {
+            if (data[i].id === itemToAdd.id) {
+                if (data[i].detail !== itemToAdd.detail) {
+                    break;
+                }
+
+                allReadyInBasket = true;
+                if (data[i].quantity !== itemToAdd.quantity) {
+                    data[i].quantity += itemToAdd.quantity
+                }
+            }
+        }
+    }
+
+    if (allReadyInBasket === false) {
+        data.push(itemToAdd)
+    }
+
+    localStorage.setItem("basket", JSON.stringify(data));
+}
+
+function removeInBasket(itemToRemove){
+    
+}
