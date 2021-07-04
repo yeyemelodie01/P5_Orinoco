ed79cf8c81906b4421dd3c7095f29868717bea45
melodieyeye <yeyemelodie@outlook.fr>
2021-07-04 13:29:48 +0200

    edit page

--- js/order_confirmation.js
+++ js/order_confirmation.js
@@ -9,15 +9,13 @@ function renderConfirm() {
     let contact = localStorage.getItem('contact');
     let totalPrice = localStorage.getItem('totalPrice');
     console.log(orderId, contact, totalPrice);
-    console.log(contact.firstname);
 
                 let productNumber = createDomElement({
                     tagName: 'p',
                     classList: 'textProduct',
-                    textNode: "Merci pour votre commande n°"+ orderId +" , avec un montant de "+ totalPrice +" a bien été enregistré",
+                    textNode: "Merci pour votre commande n°"+ orderId +" , avec un montant de "+ totalPrice +" a bien été enregistré.",
                 });
 
                 let confirmMain = document.getElementById("orderconfirm");
                 appendElementTo(confirmMain, productNumber);
-    }
-//<p>Merci pour votre commande n°123456, avec un montant de 850€ a bien été enregistré</p>
+}
